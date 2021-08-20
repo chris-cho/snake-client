3fff3bd8dfc5140b7d4fe177eca2ddddd3f02706
Chris <hyunsangc@gmail.com>
2021-08-20 01:52:58 +0000

    experimenting with ups and downs

--- client.js
+++ client.js
@@ -12,6 +12,7 @@ const connect = () => {
     conn.write('Name: HSC')
     conn.setEncoding('utf8');
     console.log("Successfully connected to game server" );
+ 
   });
   return conn;
 }
@@ -19,3 +20,8 @@ const connect = () => {
 module.exports = {
   connect,
 }
+
+/* "Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left) */
