c92d0693b0b7dfc4cb9d4380568a636aed7a7bc0
Chris <hyunsangc@gmail.com>
2021-08-20 00:10:46 +0000

    making snek game

---
+++ play.js
@@ -0,0 +1,15 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: '50541'
+  });
+
+  conn.setEncoding('utf8');
+
+  return conn;
+}
+
+console.log("Connecting ...");
+connect();
